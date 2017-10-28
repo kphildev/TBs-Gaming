190a054de23e005110ea7d9d1841ca3a0a126033
kphildev <k.phillips.dev@gmail.com>
2017-10-28 11:57:02 -0400

    get rid of non functioning code

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 $('document').ready(function () {
 
-    if ($('img').hasParent('a')) {
-        $('img').height('40px');
-    } else {
-        console.log('Image not in nav bar');
-    }
-
-
+   
 })
