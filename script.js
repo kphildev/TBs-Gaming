5835d51636c21b1442944225bf3c6cf9b30ba55e
kphildev <k.phillips.dev@gmail.com>
2017-10-28 12:05:58 -0400

    begin script framework for front end

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $('document').ready(function () {
 
+    //By default hide everything but landing page
+
+    $('#games').hide();
+    $('#download').hide();
+    $('#contact').hide();
+
+    
    
 })
